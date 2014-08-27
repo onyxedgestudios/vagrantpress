fd5bb59c95e352e505edee7e6dfa74db2bd7b1f1
Tim(OES) <tim.pickerill@onyxedgestudios.com>
2014-08-26 21:56:24 -0500

    stuff

--- wordpress/wp-content/themes/_s/header.php
+++ wordpress/wp-content/themes/_s/header.php
@@ -15,7 +15,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 <?php wp_head(); ?>
 </head>
 
